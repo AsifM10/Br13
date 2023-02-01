c6c4fbe21332d3bb70ff8d538e213f95e1f3b54d
Asif Shaikh <113437895+AsifM10@users.noreply.github.com>
2023-02-02 00:48:52 +0530

    Update first.java

--- first.java
+++ first.java
@@ -2,5 +2,6 @@ public class first {
     public static void main(String[] args) {
         System.out.println("Github tutorial");
         System.out.println("Awesome");
+        System.out.println("New");
     }
 }
